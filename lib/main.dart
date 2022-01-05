340b601d5b688f3b13004d8026e1fe4f028975d4
Julio <jucesargois@gmail.com>
2022-01-05 03:51:28 -0300

    Update main.dart
    Inserted new widgets and altereted stateless.

--- lib/main.dart
+++ lib/main.dart
@@ -9,86 +9,111 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
 
   void decrement() {
-    print(decrement);
+    setState(() {
+      count--;
+      print(count);
+    });
   }
 
   void increment() {
-    print(increment);
+    setState(() {
+      count++;
+      print(count);
+    });
   }
 
+  bool get isEmpty => count == 0;
+  bool get isFull => count == 20;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Pode entrar',
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.white,
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(32.0),
-            child: Text(
-              '0',
-              style: TextStyle(
-                fontSize: 100,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/imagem_contadorapp.jpg'),
+              fit: BoxFit.cover),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+             Text(
+              isFull ? 'Lotado' :'Pode entrar',
+              style:const TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w700,
                 color: Colors.white,
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: decrement,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(100, 100),
-                  primary: Colors.black,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24),
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Text(
+                count.toString(),
+                style:  TextStyle(
+                  fontSize: 100,
+                  fontWeight: FontWeight.w700,
+                  color: isFull ?Colors.red : Colors.white,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: isEmpty ? null : decrement,
+                  style: TextButton.styleFrom(
+                    backgroundColor: isEmpty ? Colors.white10:Colors.white,
+                    fixedSize: const Size(100, 100),
+                    primary: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  child: const Text(
+                    'Saiu',
+                    style: TextStyle(color: Colors.black, fontSize: 16),
                   ),
                 ),
-                child: const Text(
-                  'Saiu',
-                  style: TextStyle(color: Colors.black, fontSize: 16),
+                const SizedBox(
+                  width: 32,
                 ),
-              ),
-              const SizedBox(
-                width: 32,
-              ),
-              TextButton(
-                onPressed: increment,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(100, 100),
-                  primary: Colors.black,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24),
+                TextButton(
+                  onPressed: isFull ? null : increment,
+                  style: TextButton.styleFrom(
+                    backgroundColor: isFull ? Colors.white10 :Colors.white,
+                    fixedSize: const Size(100, 100),
+                    primary: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  child: const Text(
+                    'Entrou',
+                    style: TextStyle(color: Colors.black, fontSize: 16),
                   ),
                 ),
-                child: const Text(
-                  'Entrou',
-                  style: TextStyle(color: Colors.black, fontSize: 16),
-                ),
-              ),
-            ],
-          )
-        ],
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
